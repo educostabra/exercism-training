995cbd1848876b40448f90a2d654b6ebcbec59a7
eduardo costa <educostabra@gmail.com>
2019-01-29 22:13:30 -0200

    Unsolved exercise rna-transcription

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,7 +1,7 @@
 object Hamming {
 
     fun compute(dna1: String, dna2: String) = if (dna1.length == dna2.length)
-        (0 until dna1.length).count { i -> dna1[i] != dna2[i] }
+        (0 until dna1.length).count { index -> dna1[index] != dna2[index] }
     else
         throw IllegalArgumentException("left and right strands must be of equal length.")
 

ed6e2a4f76cd60bddb70f7709760f8b3ff72dd6d
eduardo costa <educostabra@gmail.com>
2019-01-29 22:34:54 -0200

    Firt soluction to RNA exercise

--- kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,1 +1,6 @@
-fun transcribeToRna(dna: String): String = ""
+fun transcribeToRna(dna: String): String {
+    var equivalence = mapOf('G' to 'C', 'C' to 'G', 'T' to 'A', 'A' to 'U')
+    var rna = ""
+    dna.forEach { rna = rna.plus(equivalence[it]) }
+    return rna
+}

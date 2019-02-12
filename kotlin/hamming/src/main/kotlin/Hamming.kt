2ec1a4d81841ecf31268bfdb806b5de72468c6a9
eduardo costa <educostabra@gmail.com>
2019-02-11 22:39:08 -0200

    New solution for Hamming exercise

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,6 +1,28 @@
 object Hamming {
 
+    /**
+     * #3 try:
+     * Solution Using String.zip to create a Pair of char.
+     */
     fun compute(dna1: String, dna2: String) = if (dna1.length == dna2.length)
+        dna1.zip(dna2).count { pair -> pair.first != pair.second }
+    else
+        throw IllegalArgumentException("left and right strands must be of equal length.")
+
+    /**
+     * #2 try:
+     * Solution Using String.filterIndexed to count the nucleotides
+     */
+    fun computeUsingOnlyOneIndex(dna1: String, dna2: String) = if (dna1.length == dna2.length)
+        dna1.filterIndexed { index, ch -> dna2[index] != ch }.length
+    else
+        throw IllegalArgumentException("left and right strands must be of equal length.")
+
+    /**
+     * #1 try:
+     * Solution Iterating over the two string simultaneous
+     */
+    fun computeIteratingOverAllValues(dna1: String, dna2: String) = if (dna1.length == dna2.length)
         (0 until dna1.length).count { index -> dna1[index] != dna2[index] }
     else
         throw IllegalArgumentException("left and right strands must be of equal length.")
--- kotlin/isogram/src/main/kotlin/Isogram.kt
+++ kotlin/isogram/src/main/kotlin/Isogram.kt
@@ -1,14 +1,43 @@
+/**
+ * 3 different solution
+ */
 object Isogram {
+
     /**
-     * The solution:
+     * The solution using Hashset
+     * 1. Transform the string to lowercase due the case insensitive checking
+     * 2. Replace characters that can be repeated
+     * 3. create a HashSet where the elements are characters without be repeated
+     * 4. Compare the size of the HashSe with the word´s length.
+     * If the values are equals there is no repetition and the word is a Isogram
+     */
+    fun isIsogram2(word: String): Boolean {
+        var wordCleaned = word.toLowerCase()
+                .replace(oldValue = " ", newValue = "")
+                .replace(oldValue = "-", newValue = "")
+        return wordCleaned.toHashSet().size == wordCleaned.length
+    }
+
+    /**
+     * The solution using toCharArray and distinct.
+     * The same principle of the solution using the HashSet
+     */
+    fun isIsogram(word: String): Boolean {
+        var wordCleaned = word.toLowerCase()
+                .replace(oldValue = " ", newValue = "")
+                .replace(oldValue = "-", newValue = "")
+        return wordCleaned.toCharArray().distinct().size == wordCleaned.length
+    }
+    /**
+     * The solution using list and group by:
      * 1. Transform the string to lowercase due the case insensitive checking
      * 2. Replace characters that can be repeated
      * 3. Create a list of characters from the string
      * 4. Group the list by character where the output will be a Map where the key is the character and the value a List of the same character
      * 5. Count all the values into the map (the List) which have more than one element.
-     * Where there is no values with site higher than 1 the word is a Isogram.
+     * If the no values with size higher than one there is no repetition and teh word is a Isogram.
      */
-    fun isIsogram(word: String) =
+    fun isIsogram1(word: String) =
             word.toLowerCase()
                     .replace(oldValue = " ", newValue = "")
                     .replace(oldValue = "-", newValue = "")

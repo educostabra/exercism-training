57de4134fa309248e9ddfdac5c939e880842ba30
eduardo costa <educostabra@gmail.com>
2019-03-07 23:42:57 -0300

    End solution of Luhn

--- kotlin/luhn/src/main/kotlin/Luhn.kt
+++ kotlin/luhn/src/main/kotlin/Luhn.kt
@@ -1,32 +1,24 @@
 object Luhn {
 
-    fun isValid(numberString: String): Boolean {
-
-        if (numberString.contains(Regex("\\p{Punct}")) || numberString.contains(Regex("\\p{Alpha}")))
-            return false
-
-        var numbers = numberString
-            .filter { digit -> digit.isDigit() }
-            .map { digit -> digit.toNumber() }
-            .toIntArray()
-
-        if (numbers.isEmpty() || numbers.size == 1)
-            return false
-
-        for (index in numbers.size - 2 downTo 0 step 2) {
-            numbers[index] = double(numbers[index])
-        }
-
-        return numbers.reduce { sum, value -> sum + value } % 10 == 0
-
-    }
+    fun isValid(numbersString: String): Boolean = numbersString
+        .let { if (it.hasLettersOrPonctuation()) return false else it }
+        .filter { it.isDigit() }
+        .map { it.toNumber() }
+        .let { if (it.isEmpty() || it.size == 1) return false else it }
+        .reversed()
+        .mapIndexed { index, digit -> if (index.isEven()) digit else compute(digit) }
+        .sum()
+        .rem(10) == 0
 
     /** Extension Function */
     private fun Char.toNumber() = Character.getNumericValue(this)
 
+    private fun Int.isEven() = this % 2 == 0
 
-    private fun double(number: Int): Int {
-        var value = number * 2
-        return if (value > 9) value - 9 else value
+    private fun String.hasLettersOrPonctuation() = this.contains(Regex("\\p{Punct}|\\p{Alpha}"))
+
+    private fun compute(number: Int): Int {
+        var doubledValue = number * 2
+        return if (doubledValue > 9) doubledValue - 9 else doubledValue
     }
 }
